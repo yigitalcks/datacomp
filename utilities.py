562c547660e5e4530736ddb6107fb4a761ee0421
yigitalcks <yigitalkcs0@outlook.com>
2024-09-15 12:25:27 +0300

    final

--- utilities.py
+++
@@ -1,7 +0,0 @@
-def tr_to_eng(df, column):
-
-    tr_eng = {'İ': 'I', 'Ü': 'U', 'Ö': 'O', 'Ğ': 'G', 'Ş': 'S', 'Ç': 'C',
-                'ü': 'u', 'ö': 'o', 'ğ': 'g', 'ş': 's', 'ç': 's'}
-    
-    for tr_char, en_char in tr_eng.items():
-        df[column] = df[column].apply(lambda x: str(x).replace(tr_char, en_char))
